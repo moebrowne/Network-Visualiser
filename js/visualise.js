16e02e34baaab1c9a49277ca19d2f632990d1272
MoeBrowne <oliver.browne@uplyme.com>
2016-01-31 17:10:25 +0000

    Replaced the size object with an interger

--- js/visualise.js
+++ js/visualise.js
@@ -8,56 +8,56 @@ var nodeArray = [
 	{
 		'name': 'Alpha',
 		'position': {x: 150,y: 150},
-		'size': {x: 10,y: 10},
+		'size': 10,
 		'subnodes': [
 			{
 				'name': 'AlphaAlpha',
-				'size': {x: 5,y: 5},
+				'size': 5,
 				distance: 30
 			},
 			{
 				'name': 'AlphaBravo',
-				'size': {x: 5,y: 5},
+				'size': 5,
 				distance: 40
 			},
 			{
 				'name': 'AlphaCharlie',
-				'size': {x: 5,y: 5},
+				'size': 5,
 				distance: 82
 			},
 			{
 				'name': 'AlphaDelta',
-				'size': {x: 5,y: 5},
+				'size': 5,
 				distance: 70
 			},
 			{
 				'name': 'AlphaEcho',
-				'size': {x: 5,y: 5},
+				'size': 5,
 				distance: 35
 			},
 			{
 				'name': 'AlphaFoxtrot',
-				'size': {x: 5,y: 5},
+				'size': 5,
 				distance: 35
 			},
 			{
 				'name': 'Charlie',
 				'position': {x: 375,y: 290},
-				'size': {x: 20,y: 20},
+				'size': 20,
 				'subnodes': [
 					{
 						'name': 'CharlieAlpha',
-						'size': {x: 14,y: 14},
+						'size': 14,
 						distance: 30
 					},
 					{
 						'name': 'CharlieBravo',
-						'size': {x: 8,y: 8},
+						'size': 8,
 						distance: 40
 					},
 					{
 						'name': 'CharlieCharlie',
-						'size': {x: 12,y: 12},
+						'size': 12,
 						distance: 82
 					}
 				]
@@ -67,7 +67,7 @@ var nodeArray = [
 	{
 		'name': 'Bravo',
 		'position': {x: 250,y: 370},
-		'size': {x: 10,y: 10}
+		'size': 10
 	}
 ];
 
@@ -88,15 +88,15 @@ function drawNodes(nodes, parent) {
 			}
 		}
 
-		canvasContext.fillRect(node.position.x, node.position.y, node.size.x, node.size.y);
+		canvasContext.fillRect(node.position.x, node.position.y, node.size, node.size);
 
 		drawHTMLNode(node);
 
 		if (typeof parent !== 'undefined') {
 			canvasContext.beginPath();
 			canvasContext.strokeStyle = '#FFF';
-			canvasContext.moveTo((parent.position.x+(parent.size.x/2)),(parent.position.y+(parent.size.y/2)));
-			canvasContext.lineTo(node.position.x+(node.size.x/2), node.position.y+(node.size.y/2));
+			canvasContext.moveTo((parent.position.x+(parent.size/2)),(parent.position.y+(parent.size/2)));
+			canvasContext.lineTo(node.position.x+(node.size/2), node.position.y+(node.size/2));
 			canvasContext.stroke();
 		}
 
@@ -109,7 +109,7 @@ function drawNodes(nodes, parent) {
 }
 
 function drawHTMLNode(node) {
-	var elem = $('<div/>').css({position: 'absolute', left: node.position.x, top: node.position.y, width: node.size.x, height: node.size.y, cursor: 'pointer'});
+	var elem = $('<div/>').css({position: 'absolute', left: node.position.x, top: node.position.y, width: node.size, height: node.size, cursor: 'pointer'});
 	$('#networkNodeMap').append(elem);
 
 }
