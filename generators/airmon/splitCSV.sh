717b80e0f826b796b7d3dd1564d623119dc21587
MoeBrowne <oliver.browne@uplyme.com>
2016-02-14 17:17:01 +0000

    Silenced csplit

--- generators/airmon/splitCSV.sh
+++ generators/airmon/splitCSV.sh
@@ -5,7 +5,7 @@ OUTPUTFILE="$HOME/output-05.csv"
 inotifywait -e modify -m "$OUTPUTFILE" | while read data; do
 
     # Split the combined CSVs into the APs and the clients
-    csplit "$OUTPUTFILE" "/Station MAC,/" "{*}" --prefix=airmondata-
+    csplit "$OUTPUTFILE" "/Station MAC,/" "{*}" --prefix=airmondata- -s
 
     # Rename the files to have more human names
     mv airmondata-00 airmondata-APs.csv
