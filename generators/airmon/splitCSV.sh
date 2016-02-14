afc43e11a062fd68d410808e879987411bf3627e
MoeBrowne <oliver.browne@uplyme.com>
2016-02-14 17:09:56 +0000

    Added some comments made made the nomenclature way better

--- generators/airmon/splitCSV.sh
+++ generators/airmon/splitCSV.sh
@@ -4,11 +4,18 @@ OUTPUTFILE="$HOME/output-05.csv"
 
 inotifywait -e modify -m "$OUTPUTFILE" | while read data; do
 
-    csplit "$OUTPUTFILE" "/Station MAC,/" "{*}"
+    # Split the combined CSVs into the APs and the clients
+    csplit "$OUTPUTFILE" "/Station MAC,/" "{*}" --prefix=airmondata-
 
-    tail -n +2 xx00 > xx00temp
-    mv xx00temp xx00
+    # Rename the files to have more human names
+    mv airmondata-00 airmondata-APs
+    mv airmondata-01 airmondata-clients
 
+    # Strip the preceding white space out of the CSV as it borks the parser
+    tail -n +2 airmondata-APs > airmondata-APs-temp
+    mv airmondata-APs-temp airmondata-APs
+
+    # Get Node to parse the data
     node airmon-parse.js
 
 done
