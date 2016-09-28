5b41065f2fa287fce253c1603ec83cf49b7bf3da
MoeBrowne <oliver.browne@uplyme.com>
2016-09-28 23:52:23 +0100

    Updated the airmon parser to emit websocket events

--- generators/airmon/airmon-parse.js
+++ generators/airmon/airmon-parse.js
@@ -1,95 +1,43 @@
+var io = require('socket.io')(3000);
 var csv = require('csv-parser');
 var fs = require('fs');
 require('datejs');
 
-var nodeData = [];
+io.on('connection', function(socket) {
+	console.log('User connected');
 
-var associated = 0;
-var unassociated = 0;
-var noap = 0;
+	fs.watchFile('airmondata-APs.csv', function(curr, prev) {
+		fs.createReadStream('airmondata-APs.csv')
+		.pipe(csv())
+		.on('data', function (data) {
 
-function getRandomIntInclusive(min, max) {
-	return Math.floor(Math.random() * (max - min + 1)) + min;
-}
+			if (typeof data['BSSID'] === 'undefined' || data[' ESSID'] === ' ' || data[' Power'] === '  -1') {
+				return;
+			}
 
-fs.createReadStream('airmondata-APs.csv')
-	.pipe(csv())
-	.on('data', function(data) {
-
-		if(typeof data['BSSID'] === 'undefined' || data[' ESSID'] === ' ' || data[' Power'] === '  -1') {
-			return;
-		}
-
-		nodeData[data['BSSID']] = {
-			"name": data[' ESSID']+' ('+data[' Power']+')',
-			"distance": (0-parseInt(data[' Power']))*5,
-			"size": 10,
-			"subnodes": []
-		};
-	})
-	.on('end', function() {
-
-		fs.createReadStream('airmondata-clients.csv')
-			.pipe(csv())
-			.on('data', function (data) {
-
-				if (typeof data[' BSSID'] ===  'undefined') {
-					return;
-				}
-
-				data[' BSSID'] = data[' BSSID'].substr(1);
-
-				// Remove unassociated clients
-				if (data[' BSSID'] === ' (not associated) ') {
-					unassociated++;
-					return;
-				}
-
-				if (typeof nodeData[data[' BSSID']] === 'undefined') {
-					noap++;
-					return;
-				}
-
-				associated++;
-
-				//console.log(nodeData[data[' BSSID']]);
-
-				nodeData[data[' BSSID']].subnodes.push({
-					"name": data['Station MAC'],
-					"size": 5,
-					"distance": (Math.max((1 - data[' Power']), 25))
-				});
-			})
-			.on('end', function () {
-
-				var allData = [];
-
-				allData.push({
-					"name": "WLAN9",
-					"position": {
-						"x": 600,
-						"y": 450
-					},
-					"size": 20,
-					"subnodes": []
-				});
-
-				Object.keys(nodeData).forEach(function (key) {
-					var val = nodeData[key];
-					allData[0].subnodes.push(val);
-				});
-
-				console.log('APs: ' + allData[0].subnodes.length);
-				console.log('Associated Clients: ' + associated);
-				console.log('Unassociated Clients: ' + unassociated);
-				console.log('APless Clients: ' + noap);
-
-				fs.writeFile('nodes.json', JSON.stringify(allData), {flags: '+w'}, function (err) {
-					if (err !== null) {
-						console.error(err);
-						throw err;
-					}
-				});
-
+			socket.emit('AP', {
+				'mac': data['BSSID'].replace(' ',''),
+				'power': data[' Power'],
+				'size': 10
 			});
+		});
 	});
+
+	fs.watchFile('airmondata-clients.csv', function(curr, prev) {
+		fs.createReadStream('airmondata-clients.csv')
+		.pipe(csv())
+		.on('data', function (data) {
+
+			if (typeof data['Station MAC'] === 'undefined' || data[' BSSID'] === ' ') {
+				return;
+			}
+
+			socket.emit('client', {
+				'mac': data['Station MAC'].replace(' ', ''),
+				'AP': data[' BSSID'].replace(' ', ''),
+				'power': data[' Power'],
+				'size': 5
+			});
+		});
+	});
+});
