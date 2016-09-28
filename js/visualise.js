7d2ce0b59672ac2ce93819d7b2ed37fa5203fc4d
MoeBrowne <oliver.browne@uplyme.com>
2016-09-28 23:52:51 +0100

    Overhauled the visualiser to accept webhooks

--- js/visualise.js
+++ js/visualise.js
@@ -6,68 +6,117 @@ var canvasContext = canvas.getContext('2d');
 canvasContext.fillStyle = '#333';
 canvasContext.strokeStyle = '#FFF';
 
-$.getJSON( "generators/airmon/nodes.json", function( data ) {
-	drawNodes(data);
+var APs = {};
+
+var socket = io('http://localhost:3000');
+
+socket.on('AP', function (AP) {
+
+	AP.position = {
+		x: getRandomArbitrary(0, 1000),
+		y: getRandomArbitrary(0, 500)
+	};
+
+	if (typeof APs[AP['mac']] === 'undefined') {
+		APs[AP['mac']] = AP;
+		APs[AP['mac']].clients = {};
+		drawNode(AP);
+	}
+
 });
 
-function drawNodes(nodes, parent) {
+socket.on('client', function (client) {
+	if (typeof APs[client['AP']] === "undefined") {
+		//drawNode(client);
+		return;
+	}
 
-	for (var i=0; i<nodes.length; i++) {
+	if (typeof APs[client['AP']]['clients'][client['mac']] === 'undefined') {
+		APs[client['AP']]['clients'][client['mac']] = client;
+		drawNode(client, APs[client['AP']]);
+	}
+});
 
-		var node = nodes[i];
 
-		// If a position hasn't be defined arrange all the sub nodes equally around the parent
-		if (typeof node.position === 'undefined' && typeof parent !== 'undefined') {
-			var angleDeg = ((360/nodes.length)*i)-90;
-			node.position = {
-				x: parent.position.x + (node.distance * Math.cos(toRadians(angleDeg))),
-				y: parent.position.y + node.distance * Math.sin(toRadians(angleDeg))
-			}
-		}
+function drawNode(node, parent) {
 
-		// If this node is a child offset it so it aligns with the parent
-		if (typeof parent !== 'undefined') {
-			node.position.x += (node.size/2);
-			node.position.y += (node.size/2);
-		}
+	node.distance = 50;
 
-		// Calculate the centre of the node
-		node.centre = {
-			x: node.position.x+(node.size/2),
-			y: node.position.y+(node.size/2)
+	// If a position hasn't be defined arrange all the sub node equally around the parent
+	if (typeof parent !== 'undefined') {
+
+		var clientCount = Object.keys(parent.clients).length;
+
+		var angleDeg = ((360 / (clientCount+1)) * getRandomArbitrary(0, 60)) - 90;
+		node.positionOffset = {
+			x: node.distance * Math.cos(toRadians(angleDeg)),
+			y: node.distance * Math.sin(toRadians(angleDeg))
 		};
 
-		// Draw the HTML element mask
-		drawHTMLNode(node);
-
-		// Check if this node is a child element and therefore needs linking to its parent
-		if (typeof parent !== 'undefined') {
-			canvasContext.beginPath();
-			canvasContext.moveTo((parent.position.x+(parent.size/2)),(parent.position.y+(parent.size/2)));
-			canvasContext.lineTo(node.centre.x, node.centre.y);
-			canvasContext.stroke();
-		}
-
-		// Check if there are children to draw
-		if (typeof node.subnodes !== 'undefined') {
-			drawNodes(node.subnodes, node);
-		}
-
-		// Draw the node
-		canvasContext.rect(node.position.x, node.position.y, node.size, node.size);
-		canvasContext.stroke();
-		canvasContext.fill();
-
+		node.position = {
+			x: parent.position.x + node.positionOffset.x,
+			y: parent.position.y + node.positionOffset.y
+		};
 	}
+
+	// Calculate the centre of the node
+	node.centre = {
+		x: node.position.x+(node.size/2),
+		y: node.position.y+(node.size/2)
+	};
+
+	// Draw the HTML element mask
+	drawHTMLNode(node);
+
+	// Check if this node is a child element and therefore needs linking to its parent
+	if (typeof parent !== 'undefined') {
+		canvasContext.beginPath();
+		canvasContext.moveTo((parent.position.x+(parent.size/2)),(parent.position.y+(parent.size/2)));
+		canvasContext.lineTo(node.centre.x, node.centre.y);
+		canvasContext.stroke();
+	}
+
+	// Check if there are children to draw
+	if (typeof node.clients !== 'undefined') {
+		for (var i=0; i<node.clients.length; i++) {
+			drawNode(node.clients[i], node);
+		}
+	}
+
+	// Draw the node
+	canvasContext.rect(node.position.x, node.position.y, node.size, node.size);
+	canvasContext.stroke();
+	canvasContext.fill();
 
 }
 
 function drawHTMLNode(node) {
-	var elem = $('<div/>').css({position: 'absolute', left: node.position.x, top: node.position.y, width: node.size, height: node.size, cursor: 'pointer'}).attr('title', node.name);
+	var elem = $('<div/>')
+		.css({position: 'absolute', left: node.position.x, top: node.position.y, width: node.size, height: node.size, cursor: 'pointer'})
+		.attr('title', node.name)
+		.attr('id', node.id)
+		.attr('draggable', true);
 	$('#networkNodeMap').append(elem);
+
+	document.addEventListener('dragexit', function(e) {
+		var node = nodeLookup[this.getAttribute('id')];
+
+		console.log(node);
+
+		node.position = {
+			x: e.pageX,
+			y: e.pageY
+		};
+
+		drawAllNodes();
+	}, false);
 
 }
 
 function toRadians (angle) {
 	return angle * (Math.PI / 180);
+}
+
+function getRandomArbitrary(min, max) {
+	return Math.floor(Math.random() * (max - min + 1)) + min;
 }
