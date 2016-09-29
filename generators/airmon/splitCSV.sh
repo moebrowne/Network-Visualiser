997d3b5e62c485151e9e0692f1600a3108d6764f
MoeBrowne <oliver.browne@uplyme.com>
2016-09-29 22:44:36 +0100

    Replaced mv with redirects

--- generators/airmon/splitCSV.sh
+++ generators/airmon/splitCSV.sh
@@ -8,12 +8,8 @@ inotifywait -e modify -m "$OUTPUTFILE" | while read data; do
     csplit "$OUTPUTFILE" "/Station MAC,/" "{*}" --prefix=airmondata- -s
 
     # Rename the files to have more human names
-    mv airmondata-00 airmondata-APs.csv
-    mv airmondata-01 airmondata-clients.csv
-
-    # Strip the preceding white space out of the CSV as it borks the parser
-    tail -n +2 airmondata-APs.csv > airmondata-APs-temp.csv
-    mv airmondata-APs-temp.csv airmondata-APs.csv
+    cat airmondata-00 | tail -n +2 > airmondata-APs.csv
+    cat airmondata-01 > airmondata-clients.csv
 
     echo "Files changed"
 
