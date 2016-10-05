1ad0173779e3059f8deafe7ff08a60264c8a3456
MoeBrowne <oliver.browne@uplyme.com>
2016-10-05 22:10:32 +0100

    Updated the iwlist generator to include the websockets lib and started correcting the data output

--- generators/iwscan/iwscan.js
+++ generators/iwscan/iwscan.js
@@ -1,37 +1,43 @@
 'use strict';
 
+var io = require('socket.io')(3000);
 var iwconfig = require('wireless-tools/iwconfig');
 var iwlist = require('wireless-tools/iwlist');
 var fs = require('fs');
 
-var interfaceName = 'wlan9';
+var interfaceName = 'wlan0';
 
+var APs = {};
 var data = [];
+
+io.on('connection', function(socket) {
+	console.log('User connected');
+});
 
 iwconfig.status(interfaceName, function(err, status) {
 
-	var networkArray = [];
-
-	data.push({
-		"name": interfaceName,
-		"position": {"x": 550,"y": 350},
-		"size": 10
-	});
-
 	iwlist.scan(interfaceName, function(err, networks) {
+		console.log(networks);
 
-		for (var i=0;i<networks.length;i++) {
-			networkArray.push({
-				"name": networks[i].ssid,
-				"size": (networks[i].channel*2),
-				"distance": (-(networks[i].signal*2))
-			});
+		for (var i=0; i < networks.length; i++) {
+
+			var AP = {
+				'mac': networks[i].address,
+				'SSID': networks[i].ssid,
+				//'power': data[' Power'],
+				//'active': (secondsLastSeen < 120),
+				'encryption': networks[i].security,
+				'size': Math.max(10,Math.round((60-parseInt(networks[i].signal))/3))
+			};
+
+			if (JSON.stringify(AP) === JSON.stringify(APs[AP.SSID])) {
+				return;
+			}
+
+			APs[AP.SSID] = AP;
+
+			console.log(networks[i]);
 		}
-
-		data[0].subnodes = networkArray;
-
-		console.log(data);
-		fs.writeFile('iwlistnodes.json', JSON.stringify(data), { flags: '+w' });
 
 	});
 
