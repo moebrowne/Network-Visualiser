90277838545186c36d701eba07d37a411d1c06f9
MoeBrowne <oliver.browne@uplyme.com>
2016-10-09 21:28:24 +0100

    Added and included a debounce lib

---
+++ js/debounce.js
@@ -0,0 +1,2 @@
+// https://snippetrepo.com/snippets/basic-vanilla-javascript-throttlingdebounce
+function debounce(a,b,c){var d;return function(){var e=this,f=arguments;clearTimeout(d),d=setTimeout(function(){d=null,c||a.apply(e,f)},b),c&&!d&&a.apply(e,f)}}
